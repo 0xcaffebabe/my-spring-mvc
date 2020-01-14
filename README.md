6fca39afb85c5e6c7433bad38d7d21b78a8449e5
MY <715711877@qq.com>
2020-01-14 11:42:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-spring-mvc
+
+实现自己的MVC框架
---
+++ src/main/java/wang/ismy/spring/mvc/Main.java
@@ -0,0 +1,11 @@
+package wang.ismy.spring.mvc;
+
+/**
+ * @author MY
+ * @date 2020/1/14 11:39
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
